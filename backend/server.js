4912dc92180727e6144672f52f54763890dc9441
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-11-15 12:30:28 -0500

    working on structure of the code

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express'); 
+const app = express(); 
+
+require('dotenv').config(); 
+
+// set up middleware 
+app.use(express.json()); 
+
+const PORT = process.env.PORT;  
+
+app.get('/', (req, res) => {
+    res.json({ 
+        BackendResponse: "server is running"
+    }); 
+}); 
+
+app.listen(PORT, () => {
+    console.log()
+})
